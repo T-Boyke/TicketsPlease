8700c49b940fd327b7259f4d3c1f31c0324b525d
Tobias <Tobias.Boyke@proton.me>
2026-03-23 10:25:01 +0100

    test(unit): resolve documentation and arrangement warnings

--- tests/TicketsPlease.UnitTests/SampleUnitTests.cs
+++ tests/TicketsPlease.UnitTests/SampleUnitTests.cs
@@ -1,13 +1,24 @@
-using FluentAssertions;
-using Xunit;
+// <copyright file="SampleUnitTests.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
-namespace TicketsPlease.UnitTests;
-
-public class SampleUnitTests
+namespace TicketsPlease.UnitTests
 {
-    [Fact]
-    public void ShoudWork()
+    using FluentAssertions;
+    using Xunit;
+
+    /// <summary>
+    /// Eine Beispiel-Testklasse zur Validierung der Testumgebung.
+    /// </summary>
+    public class SampleUnitTests
     {
-        true.Should().BeTrue();
+        /// <summary>
+        /// Ein einfacher Test, der sicherstellt, dass FluentAssertions funktioniert.
+        /// </summary>
+        [Fact]
+        public void ShoudWork()
+        {
+            true.Should().BeTrue();
+        }
     }
 }
